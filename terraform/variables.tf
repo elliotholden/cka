9f94fbf39fe2d731c1bf02636062ffc9945452fc
Elliot Holden <elliot@elliotmywebguy.com>
2025-12-01 15:43:08 +0000

    Added the username prefix to the SSH public key. Without it, Google Cloud will not create the Linux account on the remote host

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,11 +25,11 @@ variable "subnet" {
 variable "ssh-public-key-1" {
   description = "SSH public key 1"
   type        = string
-  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOuvHz4ovrcF3Uj2B9X7Jwgt9VV1wDR6KNRR433zJGx4 elliot"
+  default     = "elliot:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOuvHz4ovrcF3Uj2B9X7Jwgt9VV1wDR6KNRR433zJGx4"
 }
 
 variable "ssh-public-key-2" {
   description = "SSH public key 2"
   type        = string
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2icGmmoZi1OgKVS37SIquk0MO+FWtZBjxRK+tLKxd5h3hacuqZ4SkWWRrd1K2nRO3uazKZASEUo6ksvDDqpviQZOeVsA/9Q0JYXNSB9IRahebgId12beII+kbEDl57wb9czb8K4dMCuU0Rn8mqmaBjM1ruPGaocO7oGyGJEgEUEMJwiKHzz0EfK2QcwDVQ2IExcOAiS3g2SP/pSVkqZjjnwXoO56d6yHAGGp/XpzHu9OW03KNnSlgi9kuQSSwetzTCOUDRbXG0Z1Is4AAdKrSzQ2RkUIVZalUtRAeUSjiWMD4lu4yEdRnQE36Arz3us11CowUb+m44d2u4Qm088Ul elliot"
+  default     = "elliot:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2icGmmoZi1OgKVS37SIquk0MO+FWtZBjxRK+tLKxd5h3hacuqZ4SkWWRrd1K2nRO3uazKZASEUo6ksvDDqpviQZOeVsA/9Q0JYXNSB9IRahebgId12beII+kbEDl57wb9czb8K4dMCuU0Rn8mqmaBjM1ruPGaocO7oGyGJEgEUEMJwiKHzz0EfK2QcwDVQ2IExcOAiS3g2SP/pSVkqZjjnwXoO56d6yHAGGp/XpzHu9OW03KNnSlgi9kuQSSwetzTCOUDRbXG0Z1Is4AAdKrSzQ2RkUIVZalUtRAeUSjiWMD4lu4yEdRnQE36Arz3us11CowUb+m44d2u4Qm088Ul"
 }
