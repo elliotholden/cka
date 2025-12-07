0e98a2ee16201f0e5113292730ee498fa4156f1e
Elliot Holden <elliot@elliotmywebguy.com>
2025-12-07 00:23:12 +0000

    update

--- terraform/simple/main.tf
+++ terraform/simple/main.tf
@@ -98,40 +98,11 @@ resource "google_compute_instance" "w2" {
   }
 }
 
-resource "google_compute_instance" "w3" {
-
-  name            = "worker-3-simple"
-  hostname        = "w3-simple.cka"
-  machine_type    = "e2-small"
-  can_ip_forward  = true
-  tags            = ["http-server"]
-
-  boot_disk {
-    initialize_params {
-      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2404-noble-amd64-v20251121"
-      size  = 20
-      type  = "pd-ssd"
-    }
-  }
-
-
-  metadata = {
-    enable-osconfig = "TRUE"
-    ssh-keys        = "elliot:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOuvHz4ovrcF3Uj2B9X7Jwgt9VV1wDR6KNRR433zJGx4\nemwg:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOuvHz4ovrcF3Uj2B9X7Jwgt9VV1wDR6KNRR433zJGx4"
-  }
-
-  network_interface {
-    access_config {}
-    subnetwork  = "projects/k8s-simple-480315/regions/us-east1/subnetworks/default"
-  }
-}
-
 output "all_instance_ips" {
   value = {
     control_node = google_compute_instance.c1.network_interface[0].access_config[0].nat_ip
     worker_1     = google_compute_instance.w1.network_interface[0].access_config[0].nat_ip
     worker_2     = google_compute_instance.w2.network_interface[0].access_config[0].nat_ip
-    worker_3     = google_compute_instance.w3.network_interface[0].access_config[0].nat_ip
   }
   description = "All instance public IP addresses"
 }
